3a6f7e7d6a3a6870e255cd1bbaf1bf0e9e7ef3bb
flaviooracle <flaviooracle@yahoo.com.br>
2024-12-18 21:11:14 -0300

    ok

--- target/generated-sources/annotations/com/example/bancoH2/adapters/out/jpa/entities/pessoa/mapper/PessoaMapperImpl.java
+++ target/generated-sources/annotations/com/example/bancoH2/adapters/out/jpa/entities/pessoa/mapper/PessoaMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-15T22:13:28-0300",
+    date = "2024-12-18T21:10:26-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
